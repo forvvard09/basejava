988eed0dd8ca2a4c01b2ff3ddc7a4e3338cb0672
Sergei Poddubniak <forvvard09@gmail.com>
2019-03-20 11:39:24 +0300

    Done hw05 part 1.ver 2.0, corrections on the comments

--- test/main/java/app/storage/AbstractArrayStorageTest.java
+++ test/main/java/app/storage/AbstractArrayStorageTest.java
@@ -9,7 +9,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 
-public abstract class AbstractAлrrayStorageTest {
+public abstract class AbstractArrayStorageTest {
     private Storage storage;
     private static final String UUID_1 = "uuid1";
     private static final String UUID_2 = "uuid2";
