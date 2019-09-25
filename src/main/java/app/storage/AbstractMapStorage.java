b38a9363a4618ee7a317cbf22604e64089b11559
Sergei Poddubniak <forvvard09@gmail.com>
2019-09-25 12:05:19 +0300

    Lesson07. Generics. Update. Add generics to class AbstractMapStorage.

--- src/main/java/app/storage/AbstractMapStorage.java
+++ src/main/java/app/storage/AbstractMapStorage.java
@@ -4,12 +4,12 @@ import main.java.app.model.Resume;
 
 import java.util.*;
 
-public abstract class AbstractMapStorage extends AbstractStorage {
+public abstract class AbstractMapStorage<T> extends AbstractStorage<T> {
 
     protected final Map<String, Resume> mapStorage = new HashMap<>();
 
     @Override
-    protected void doSave(Object key, Resume newResume) {
+    protected void doSave(T key, Resume newResume) {
         mapStorage.put(newResume.getUuid(), newResume);
     }
 
