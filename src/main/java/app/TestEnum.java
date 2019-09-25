9737ea7e6470e37592562c7d08c970727a715204
Sergei Poddubniak <forvvard09@gmail.com>
2019-09-25 14:54:59 +0300

    Lesson07. Enum.

---
+++ src/main/java/app/TestEnum.java
@@ -0,0 +1,20 @@
+package main.java.app;
+
+import main.java.app.model.SectionType;
+
+public class TestEnum {
+    public enum Singleton {
+        INSTANCE
+    }
+
+    public static void main(String[] args) {
+
+        Singleton instance = Singleton.valueOf("INSTANCE");
+        System.out.println(instance);
+        System.out.println(instance.ordinal()); //выводит порядковый номер
+        System.out.println();
+        for(SectionType type : SectionType.values()) {
+            System.out.println(type.getTitle());
+        }
+    }
+}
