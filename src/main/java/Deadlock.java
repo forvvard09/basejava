81c30a6948915d9d8c844dec24bbab720bd810d4
Sergei Poddubniak <forvvard09@gmail.com>
2019-12-23 11:48:17 +0300

    Lesson12. Analysis of last homework.

--- src/main/java/Deadlock.java
+++ src/main/java/Deadlock.java
@@ -1,12 +1,21 @@
 package main.java;
 
-import static java.lang.Thread.State.BLOCKED;
-
 public class Deadlock {
 
     private final Object lock1 = new Object();
     private final Object lock2 = new Object();
 
+    public static void main(String[] args) {
+
+        Deadlock deadlock = new Deadlock();
+
+        Thread th1 = new Thread(() -> deadlock.doLock(deadlock.lock1, deadlock.lock2));
+
+        Thread th2 = new Thread(() -> deadlock.doLock(deadlock.lock2, deadlock.lock1));
+
+        th1.start();
+        th2.start();
+    }
 
     public void doLock(Object lock1, Object lock2) {
         synchronized (lock1) {
@@ -18,21 +27,5 @@ public class Deadlock {
             synchronized (lock2) {
             }
         }
-    }
-
-    public static void main(String[] args) {
-
-        Deadlock deadlock = new Deadlock();
-
-        Thread th1 = new Thread(() -> {
-            deadlock.doLock(deadlock.lock1, deadlock.lock2);
-        });
-
-        Thread th2 = new Thread(() -> {
-            deadlock.doLock(deadlock.lock2, deadlock.lock1);
-        });
-
-        th1.start();
-        th2.start();
     }
 }
--- src/main/java/MyConcurrency.java
+++ src/main/java/MyConcurrency.java
@@ -7,7 +7,6 @@ import java.util.List;
 
 public class MyConcurrency {
     private static final int THREADS_NUMBER = 10000;
-    private Object LOCK = new Object();
     private static int counter;
 
     public static void main(String[] args) throws InterruptedException {
@@ -66,6 +65,30 @@ public class MyConcurrency {
 //        Thread.sleep(999);
         LazySingleton.getInstance();
         System.out.println(counter);
+
+
+        final String lock1 = "lock1";
+        final String lock2 = "lock2";
+        deadLock(lock1, lock2);
+        deadLock(lock2, lock1);
+    }
+
+    private static void deadLock(Object lock1, Object lock2) {
+        new Thread(()-> {
+            System.out.println("Waiting " + lock1);
+            synchronized (lock1) {
+                System.out.println("Holding " + lock1);
+                try {
+                    Thread.sleep(50);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("Waiting " + lock2);
+                synchronized (lock2) {
+                    System.out.println("Holding " + lock2);
+                }
+            }
+        }).start();
     }
 
     private  void inc() {
