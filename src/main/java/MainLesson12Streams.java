ad2d1d6529ee7949f00c893507b9e8fa6c01e28e
Sergei Poddubniak <forvvard09@gmail.com>
2020-01-10 15:10:18 +0300

    Lesson12. Concurrency.

---
+++ src/main/java/MainLesson12Streams.java
@@ -0,0 +1,25 @@
+package main.java;
+
+import java.util.List;
+
+public class MainLesson12Streams {
+
+    public static void main(String[] args) {
+
+        MainLesson12Streams lesson = new MainLesson12Streams();
+
+
+
+    }
+
+    public int minValue(int[] values) {
+        return 0;
+    }
+
+    public List<Integer> addOrEven(List<Integer> integers) {
+        return null;
+
+    }
+
+
+}
